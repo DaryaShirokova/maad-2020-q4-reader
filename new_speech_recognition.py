a1940dd5afcd50609a5b4a1b060cd00bb7d501fa
Владислав Эм <excuse.em@Vladislavs-MacBook-Pro.local>
2020-11-23 01:31:25 +0300

    updated, can be used in case we need to divide audio into pieces

--- new_speech_recognition.py
+++ new_speech_recognition.py
@@ -5,11 +5,14 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
 
-def silence_based_conversion(path):  # a function that divides the audio file into pieces
+def silence_based_conversion(path):
+    """
+    a function that divides the audio file into pieces
+    """
     song = AudioSegment.from_wav(path)
 
-    fh = open("recognised_text.txt", "w+")
-    pieces = split_on_silence(song, min_silence_len=1000, silence_thresh=-50)
+    f = open("recognised_text.txt", "w+")
+    pieces = split_on_silence(song, min_silence_len=400, silence_thresh=-50)
     # split file where silence is at least 1 second
 
     # store little audio files
@@ -18,18 +21,16 @@ def silence_based_conversion(path):  # a function that divides the audio file in
     except(FileExistsError):
         pass
 
-    os.chdir('audio_pieces')
-
     i = 0
     for piece in pieces:
 
-        piece_silent = AudioSegment.silent(duration=1000)
+        piece_silent = AudioSegment.silent(duration=2000)
         # add a few seconds silence to the beginning and to the end of audio file
         # it's necessary, because we don't want to miss any word
         audio_piece = piece_silent + piece + piece_silent
 
         print("saving piece{0}.wav".format(i))
-        audio_piece.export("./piece{0}.wav".format(i), bitrate='192k', format="wav")
+        audio_piece.export("./audio_pieces/piece{0}.wav".format(i), bitrate='192k', format="wav")
 
         filename = 'piece' + str(i) + '.wav'
         print("Processing piece " + str(i))
@@ -43,20 +44,17 @@ def silence_based_conversion(path):  # a function that divides the audio file in
             audio_listened = r.record(source)
 
         try:
-            # try converting it to text
-            rec = r.recognize_google(audio_listened)
-            # write the output to the file.
-            fh.write(rec + ". ")
+            rec = r.recognize_sphinx(audio_listened)
+            f.write(rec + ". ")
 
         except sr.UnknownValueError:
-            print("didn't recognise")
+            raise ValueError("didn't recognise")
 
         except sr.RequestError as e:
             print("smth wrong with service; {0}".format(e))
 
         i += 1
 
-    os.chdir('..')
 
 
 silence_based_conversion('resources/chapter1.wav')
